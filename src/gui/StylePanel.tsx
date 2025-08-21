eb191284e2b92605855cf6eb3fb1609f4d9b06ea
Aleks Kissinger <aleks0@gmail.com>
2025-08-21 23:09:20 +0100

    showing styles

--- src/gui/StylePanel.tsx
+++ src/gui/StylePanel.tsx
@@ -1,3 +1,4 @@
+import { colorToHex } from "../lib/color";
 import Styles from "../lib/Styles";
 import { GraphTool } from "./GraphEditor";
 import Toolbar from "./Toolbar";
@@ -31,6 +32,35 @@ const StylePanel = ({
       <i>[{tikzStyles.filename !== "" ? tikzStyles.filename : "no tikzstyles"}]</i>
 
       <br />
+
+      <div style={{ overflow: "auto" }}>
+        {tikzStyles.styleData.entrySeq().map(([name, style]) => {
+          const fill = colorToHex(
+            style.property("tikzit fill") ?? style.property("fill") ?? "white"
+          );
+          const draw = colorToHex(
+            style.property("tikzit draw") ?? style.property("draw") ?? "black"
+          );
+          const shortName = name.length > 10 ? name.slice(0, 10) + "…" : name;
+          return (
+            <a href="#" title={name} onClick={() => false}>
+              <svg key={name} width={50} height={50} style={{ margin: "5px" }}>
+                <circle cx={25} cy={15} r={12} fill={fill} stroke={draw} strokeWidth={2} />
+                <text
+                  x={25}
+                  y={43}
+                  textAnchor="middle"
+                  alignmentBaseline="middle"
+                  fontSize="10px"
+                  fontStyle="italic"
+                >
+                  {shortName}
+                </text>
+              </svg>
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 };
