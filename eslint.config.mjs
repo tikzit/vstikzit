4f2377b50bf1d899b988c10b4791e288848f0047
Aleks Kissinger <aleks0@gmail.com>
2025-08-26 07:40:58 +0100

    added react-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,37 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default [{
-    files: ["**/*.ts"],
+    files: ["**/*.ts", "**/*.tsx"],
 }, {
     plugins: {
         "@typescript-eslint": typescriptEslint,
+        "react": react,
+        "react-hooks": reactHooks,
     },
 
     languageOptions: {
         parser: tsParser,
         ecmaVersion: 2022,
         sourceType: "module",
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
     },
 
     rules: {
+        ...react.configs.recommended.rules,
+        ...reactHooks.configs.recommended.rules,
         "@typescript-eslint/naming-convention": ["warn", {
             selector: "import",
             format: ["camelCase", "PascalCase"],
